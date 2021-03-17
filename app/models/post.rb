06e14115273f56ab8cc94563289d2cf61ca53607
Roman Podilka <podilkaroman@gmail.com>
2021-03-17 13:26:29 +0200

    added is blank? validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  validates :title, :content, presence: true
   has_and_belongs_to_many :tags
   scope :search_result, ->(parameter) { where("lower(title) LIKE :search", search: "%#{parameter}%") }
 

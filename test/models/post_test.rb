eb1bfed8a92704a76db6afb4f087f06edca9e63e
Roman Podilka <podilkaroman@gmail.com>
2021-03-18 12:24:24 +0200

    added User model by including devise gem

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+  test "should not save a post without content" do
+    @post = Post.new
+    @post.title = "Rails and Ruby"
+    assert_not @post.save, "Saved the post without a content"
+  end
 end
